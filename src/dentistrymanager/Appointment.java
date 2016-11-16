3e8138046c6b61e90c8e5b619f62d450fdb3a72a
nsfrias <nsfrias@gmail.com>
2016-11-16 16:02:25 +0000

    Updated Appointment / Partner

--- src/dentistrymanager/Appointment.java
+++ src/dentistrymanager/Appointment.java
@@ -1,9 +1,63 @@
 package dentistrymanager;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Appointment {
 	
-	public Appointment() {
-		
+	// Private variables
+	private int appointmentID;
+	private String partner;
+	private long date;
+	private int startTime;
+	private int endTime;
+	private boolean finish;
+	
+	// Constructor
+	public Appointment(int appointmentID, String partner, long date, int startTime, int endTime, boolean finish) {
+		this.appointmentID = appointmentID;
+		this.partner = partner; 
+		this.date = date;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.finish = finish;
+	}
+	
+	// Accessors
+	public int getAppointmentID() {
+		return appointmentID;
+	}
+	
+	public String getPartner() {
+		return partner;
+	}
+	
+	public long getDate() {
+		return date;
+	}
+	
+	public int getStartTime() {
+		return startTime;
 	}
 
+	public int getEndTime() {
+		return endTime;
+	}
+	
+	public boolean isFinished() {
+		return finish;
+	}
+	
+	// Database methods
+	public boolean finish(Connection connection) {
+		try(Statement stmt = connection.createStatement()) {
+			String sql = "UPDATE Appointment SET finish = TRUE WHERE appointmentID = " + appointmentID + ";";
+			int numRows = stmt.executeUpdate(sql);
+			return numRows == 1 ? true : false;
+		} catch(SQLException e) {
+			DBConnect.printSQLError(e);
+			return false;
+		}
+	}
 }
--- src/dentistrymanager/Partner.java
+++ src/dentistrymanager/Partner.java
@@ -26,7 +26,9 @@ public class Partner {
 												+ " AND " + DateUtilities.endWeek(week)+ ";";
 			ResultSet res = stmt.executeQuery(sql);
 			while(res.next())
-				appointments.add(new Appointment());
+				appointments.add(new Appointment(res.getInt("appointmentID"), res.getString("partner"), 
+                        						 res.getLong("Date"), res.getInt("startTime"), res.getInt("endTime"), 
+                        						 res.getBoolean("finish")));
 		} catch(SQLException e) {
 			DBConnect.printSQLError(e);
 		}
@@ -40,7 +42,9 @@ public class Partner {
 												+ " AND date = "+ DateUtilities.today() + ";";
 			ResultSet res = stmt.executeQuery(sql);
 			while(res.next())
-				appointments.add(new Appointment());
+				appointments.add(new Appointment(res.getInt("appointmentID"), res.getString("partner"), 
+                                                 res.getLong("Date"), res.getInt("startTime"), res.getInt("endTime"), 
+                                                 res.getBoolean("finish")));
 		} catch(SQLException e) {
 			DBConnect.printSQLError(e);
 		}
@@ -54,7 +58,9 @@ public class Partner {
 						+ "(SELECT MIN(appointmentID) FROM Appointment WHERE partner = " + name + "AND finish = FALSE;";
 			ResultSet res = stmt.executeQuery(sql);
 			if(res.next())
-				nextAppointment = new Appointment();
+				nextAppointment = new Appointment(res.getInt("appointmentID"), res.getString("partner"), 
+						                          res.getLong("Date"), res.getInt("startTime"), res.getInt("endTime"), 
+						                          res.getBoolean("finish"));
 		} catch(SQLException e) {
 			DBConnect.printSQLError(e);
 		}
@@ -62,24 +68,18 @@ public class Partner {
 	}
 	
 	// Static methods
-	/*
-	public static Partner getPartner(Connection connection, String name) {
-				
-		Partner partner = null;
-		
+	public static ArrayList<Partner> getAll(Connection connection) {
+		ArrayList<Partner> partners = new ArrayList<>();
 		try(Statement stmt = connection.createStatement()) {
-			Appointment appointments = new Appointment();
-			String query = "SELECT * FROM Partner WHERE partnerID = "+partnerID+";";
-			ResultSet res = stmt.executeQuery(query);
-			partner = new Partner(	res.getString("name"),
-									appointments.getAppointments(connection, partnerID));
+			String sql = "SELECT * FROM Partner;";
+			ResultSet res = stmt.executeQuery(sql);
+			while(res.next())
+				partners.add(new Partner(res.getString("name")));
 		} catch (SQLException e) {
 			DBConnect.printSQLError(e);
 		} catch (Exception e) {
 			System.err.println(e);
 		}
-		
-		return partner;
+		return partners;
 	}
-	*/
 }
