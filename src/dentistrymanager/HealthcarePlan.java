28fe39458079ebe8e01248f967a67204dcd8c9ed
aca15afp <afparker1@sheffield.ac.uk>
2016-11-18 16:13:51 +0000

    Add files via upload

---
+++ src/dentistrymanager/HealthcarePlan.java
@@ -0,0 +1,62 @@
+package dentistrymanager;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class HealthcarePlan {
+	
+	// Instance variables
+	private String name;
+	private double monthlyPayment;
+	private ArrayList<Coverage> coverage;
+	
+	// Constructor
+	public HealthcarePlan(String name, double monthlyPayment,  ArrayList<Coverage> coverage) {
+		this.name = name;
+		this.monthlyPayment = monthlyPayment;
+		this.coverage = coverage;
+	}
+	
+	public HealthcarePlan(String name, double monthlyPayment) {
+		this(name, monthlyPayment, new ArrayList<>());
+	}
+	
+	// Accessors
+	public String getName() {
+		return name;
+	}
+
+	public double getMonthlyPayment() {
+		return monthlyPayment;
+	}
+	
+	public ArrayList<Coverage> getCoverage() {
+		return coverage;
+	}
+	
+	// OtherMethods
+	public String toString() {
+		
+		// Complete method ...
+		return  "";
+	}
+	
+	// Static methods
+	public static ArrayList<HealthcarePlan> getAll(Connection connection) {
+		ArrayList<HealthcarePlan> plans = new ArrayList<>();
+		try(Statement stmt = connection.createStatement()) {
+			String query = "SELECT * FROM HealthCarePlan;";
+			ResultSet res = stmt.executeQuery(query);
+			while(res != null) {
+				String plan = res.getString("name");
+				
+				// Get the plan coverage
+				ArrayList<Coverage> planCoverage = Coverage.getCoverageByPlan(connection, plan);
+				plans.add(new HealthcarePlan(plan, res.getDouble("monthlyPayment"), planCoverage));
+			}
+		} catch (SQLException e) {
+			DBConnect.printSQLError(e);
+		} 
+		return plans;
+	}
+}
